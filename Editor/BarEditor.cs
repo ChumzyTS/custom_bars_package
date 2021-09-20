e43f9122bb322b9549007a02bd9d3f4bae1952e3
ChumzyTS <45547695+ChumzyTS@users.noreply.github.com>
2021-09-20 01:01:19 -0500

    Add files via upload

---
+++ Editor/BarEditor.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEditor;
+
+[CustomEditor(typeof(Bar))]
+public class BarEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        Bar script = (Bar)target;
+
+        // Anchor Type Dropdown
+        GUIContent typeArrayLabel = new GUIContent("Anchor Position");
+        script.anchorIdx = EditorGUILayout.Popup(typeArrayLabel, script.anchorIdx, script.anchorArray);
+        script.anchor = script.anchorArray[script.anchorIdx];
+
+        // Text Display Dropdown
+        GUIContent textDisplayLabel = new GUIContent("Text Display");
+        script.textDisplay = EditorGUILayout.Toggle(textDisplayLabel, script.textDisplay);
+
+        if (script.textDisplay)
+        {
+            GUIContent textObjectLabel = new GUIContent("Text Display Object");
+            script.textObject = (Text)EditorGUILayout.ObjectField(script.textObject, typeof(Text), true);
+
+            // Text Style Dropdown
+            GUIContent textStyleLabel = new GUIContent("Text Style");
+            script.textStyleIdx = EditorGUILayout.Popup(textStyleLabel, script.textStyleIdx, script.textStyleArray);
+            script.textStyle = script.textStyleArray[script.textStyleIdx];
+
+            if (script.textStyle == "Custom")
+            {
+                GUIContent customStyleLabel = new GUIContent("Custom Style");
+                script.actualStyle = EditorGUILayout.TextField(customStyleLabel, script.actualStyle);
+            }
+            else if (script.textStyle == "[Cur][Max]")
+            {
+                script.actualStyle = "[Cur]/[Max]";
+            }
+            else
+            {
+                script.actualStyle = script.textStyle;
+            }
+
+            if (script.actualStyle != null)
+            {
+                if (script.actualStyle.Contains("[Perc]"))
+                {
+                    GUIContent decimalCountLabel = new GUIContent("Number of Decimals");
+                    script.decimalCount = Mathf.Clamp(EditorGUILayout.IntField(decimalCountLabel, script.decimalCount), 0, 4);
+                }
+            }
+        }
+        else
+        {
+            script.textObject = null;
+        }
+
+        // Set Values Dropdown
+        GUIContent setValuesLabel = new GUIContent("Manual Values");
+        script.manualValues = EditorGUILayout.Toggle(setValuesLabel, script.manualValues);
+
+        if (script.manualValues)
+        {
+            
+            GUIContent currentValueLabel = new GUIContent("Current Value");
+            script.current = EditorGUILayout.FloatField(currentValueLabel, script.current);
+
+            script.current = Mathf.Clamp(script.current, 0, script.max);
+
+            GUIContent maxValueLabel = new GUIContent("Max Value");
+            script.max = EditorGUILayout.FloatField(maxValueLabel, script.max);
+
+            // Update Button
+            if (GUILayout.Button("Reload Bar"))
+            {
+                script.UpdateBar(script.current);
+            }
+        }
+
+    }
+    
+}
---
+++ Runtime/Bar.cs
@@ -0,0 +1,146 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Bar : MonoBehaviour
+{
+    // Set Values in Editor
+    [HideInInspector]
+    public bool manualValues;
+    [HideInInspector]
+    public float max;
+    [HideInInspector]
+    public float current;
+
+    // Default Required Bar Stuff
+    public GameObject background;
+    public GameObject bar;
+
+    private RectTransform backgroundRect;
+    private RectTransform barRect;
+
+    // Anchors decide what side the bar should snap to
+    [HideInInspector]
+    public int anchorIdx;
+    [HideInInspector]
+    public string[] anchorArray = { "Left", "Middle", "Right" };
+    [HideInInspector]
+    public string anchor;
+
+    // Has Text Display
+    [HideInInspector]
+    public bool textDisplay;
+
+    [HideInInspector]
+    public Text textObject;
+
+    [HideInInspector]
+    public int textStyleIdx;
+    [HideInInspector]
+    public string[] textStyleArray = { "[Cur]", "[Cur][Max]", "[Perc]%", "Custom"};
+    [HideInInspector]
+    public string textStyle;
+    [HideInInspector]
+    public string actualStyle;
+    [HideInInspector]
+    public int decimalCount;
+
+
+    private bool validBar = false;
+
+    public void Awake()
+    {
+
+        if (background != null && bar != null)
+        {
+            GetRect();
+        }
+        else
+        {
+            Debug.LogWarning("Bar \"" + gameObject.name + "\" is missing a required bar object!");
+        }
+    }
+
+    private void GetRect()
+    {
+        backgroundRect = background.GetComponent<RectTransform>();
+        barRect = bar.GetComponent<RectTransform>();
+
+        if (backgroundRect != null && barRect != null)
+        {
+            validBar = true;
+        }
+        else
+        {
+            Debug.LogWarning("Bar \"" + gameObject.name + "\" is missing a bar RectTransform!");
+        }
+    }
+
+    public void UpdateBar(float newValue)
+    {
+        if (validBar)
+        {
+            // Set Current Value
+            current = Mathf.Clamp(newValue, 0, max);
+
+            // Calculate Size
+            float barPercent = current / max;
+            float barWidth = backgroundRect.sizeDelta.x * barPercent;
+
+            // Set Bar Size
+            barRect.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, barWidth);
+
+            // Align Bar Based Off Anchor
+            if (anchor == "Left")
+            {
+                barRect.SetPositionAndRotation(new Vector3(backgroundRect.position.x - ((backgroundRect.sizeDelta.x - barWidth) / 2), backgroundRect.position.y, backgroundRect.position.z), backgroundRect.rotation);
+            }
+            else if (anchor == "Right")
+            {
+                barRect.SetPositionAndRotation(new Vector3(backgroundRect.position.x + ((backgroundRect.sizeDelta.x - barWidth) / 2), backgroundRect.position.y, backgroundRect.position.z), backgroundRect.rotation);
+            }
+            else
+            {
+                barRect.SetPositionAndRotation(backgroundRect.position, backgroundRect.rotation);
+
+            }
+
+            // Set Text Display
+            if (textDisplay && textObject != null)
+            {
+                string displayedText = actualStyle;
+
+                displayedText = displayedText.Replace("[Cur]", current.ToString());
+                displayedText = displayedText.Replace("[Max]", max.ToString());
+
+                if (actualStyle.Contains("[Perc]"))
+                {
+                    if (decimalCount > 0)
+                    {
+                        displayedText = displayedText.Replace("[Perc]", (Mathf.Floor(barPercent * Mathf.Pow(10, decimalCount) * 100) / Mathf.Pow(10, decimalCount)).ToString());
+                    }
+                    else
+                    {
+                        displayedText = displayedText.Replace("[Perc]", Mathf.Floor(barPercent * 100).ToString());
+                    }
+                }
+
+                textObject.text = displayedText;
+            }
+        }
+    }
+
+
+    public void SetMax(float newMax, bool setCurrent = false)
+    {
+        max = newMax;
+
+        if (setCurrent)
+        {
+            current = newMax;
+        }
+
+        UpdateBar(current);
+    }
+}
